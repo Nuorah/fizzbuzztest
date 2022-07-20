f77e7da547a7708150fef13efa131e808de02c8a
Haroun Moatassime <haroun.moatassime@gmail.com>
2022-07-20 14:40:17 +0100

    ajout de tests

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use serde::Deserialize;
 async fn main() -> std::io::Result<()> {
     use actix_web::{App, HttpServer};
 
-    HttpServer::new(|| App::new().service(index))
+    HttpServer::new(|| App::new().service(fizzbuzz))
         .bind(("127.0.0.1", 8080))?
         .run()
         .await
@@ -20,7 +20,7 @@ struct FizzBuzzParams {
     str2: String,
 }
 
-fn fizzbuzz(int1: u32, int2: u32, limit: u32, str1: &str, str2: &str) -> Vec<String> {
+fn fizzbuzz_generator(int1: u32, int2: u32, limit: u32, str1: &str, str2: &str) -> Vec<String> {
     (1..limit+1).map(|i| {
         match (i%int1, i%int2) {
             (0, 0) => str1.to_owned() + &str2,
@@ -33,9 +33,9 @@ fn fizzbuzz(int1: u32, int2: u32, limit: u32, str1: &str, str2: &str) -> Vec<Str
 }
 
 #[get("/")]
-async fn index(fizzbuzzparams: web::Query<FizzBuzzParams>) -> Result<web::Json<Vec<String>>> {
+async fn fizzbuzz(fizzbuzzparams: web::Query<FizzBuzzParams>) -> Result<web::Json<Vec<String>>> {
     Ok(web::Json(
-        fizzbuzz(
+        fizzbuzz_generator(
             fizzbuzzparams.int1,
             fizzbuzzparams.int2,
             fizzbuzzparams.limit,
@@ -46,5 +46,49 @@ async fn index(fizzbuzzparams: web::Query<FizzBuzzParams>) -> Result<web::Json<V
 
 #[cfg(test)]
 mod tests {
-    todo!();
+    use actix_web::{http::header::ContentType, test, web, App, body};
+    use actix_web::rt::pin;
+
+    use super::*;
+
+    #[test]
+    async fn test_fizzbuzz_generator() {
+        let result = vec![
+            "1", "2", "fizz", "4", "buzz", "fizz", "7", "8", "fizz", "buzz",
+            "11", "fizz", "13", "14", "fizzbuzz", "16", "17", "fizz", "19", "buzz"
+        ];
+        assert_eq!(result, fizzbuzz_generator(3, 5, 20, "fizz", "buzz"))
+    }
+
+    #[actix_web::test]
+    async fn test_fizzbuzz_get() {
+        let app = test::init_service(App::new().service(fizzbuzz)).await;
+        let req = test::TestRequest::get()
+            .uri("/?int1=3&int2=5&limit=15&str1=fish&str2=bush")
+            .to_request();
+        let result: Vec<String> = test::call_and_read_body_json(&app, req).await;
+        assert_eq!(result, vec![
+            "1", "2", "fish", "4", "bush", "fish", "7", "8", "fish", "bush",
+            "11", "fish", "13", "14", "fishbush"])
+    }
+
+    #[actix_web::test]
+    async fn test_fizzbuzz_missing_param() {
+        let app = test::init_service(App::new().service(fizzbuzz)).await;
+        let req = test::TestRequest::get()
+            .uri("/?int1=3&int2=5&limit=15&str2=bush")
+            .to_request();
+        let resp = test::call_service(&app, req).await;
+        assert!(resp.status().is_client_error())
+    }
+
+    #[actix_web::test]
+    async fn test_fizzbuzz_invalid_param() {
+        let app = test::init_service(App::new().service(fizzbuzz)).await;
+        let req = test::TestRequest::get()
+            .uri("/?int1=fizz&int2=5&limit=15&str1=fizz&str2=bush")
+            .to_request();
+        let resp = test::call_service(&app, req).await;
+        assert!(resp.status().is_client_error())
+    }
 }
